0813eaf965a125af30d25c00b22f5f2a0e557951
Shaharyar <shaharyar4t4@gmail.com>
2025-09-10 22:10:29 +0500

    splash screen is done

--- app/navigation/stack/allScreen.tsx
+++ app/navigation/stack/allScreen.tsx
@@ -1,13 +1,16 @@
 
-import { Home } from '@/app/navigation/tab/Home';
+import SplashScreen from '@/app/screen/SplashScreen';
 import MainLayout from '@/components/MainLayout';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Home } from '../tab/Home';
 
 
 //this is the root stack param list which tranfer the another data 
 export type RootStackParamList = {
+  SplashScreen: undefined;
   Home: undefined;
+  
 //   About: undefined;
 //   Detialform: { id: string; name: string };
 //   Effect: undefined;
@@ -21,13 +24,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const Allscreen = () => {
   return (
     <MainLayout>
-      <Stack.Navigator initialRouteName={"Home"} screenOptions={{
-        headerShown: true,
+      <Stack.Navigator initialRouteName={"SplashScreen"} screenOptions={{
+        headerShown: false,
         // headerStyle: { backgroundColor: "#999921" },
         // headerTintColor: "#fff",
         // headerTitleStyle: { fontWeight: "bold" },
         // headerTitleAlign: "center",
       }}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="Home" component={Home} />
         {/* <Stack.Screen name="About" component={About} />
         <Stack.Screen name="Detialform" component={Detialform} options={({route})=>({
---
+++ app/screen/SplashScreen.tsx
@@ -0,0 +1,68 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useEffect } from 'react';
+
+import { Animated, StyleSheet, Text, View } from 'react-native';
+import { RootStackParamList } from '../navigation/stack/allScreen';
+
+type PropsType = NativeStackScreenProps<RootStackParamList, 'SplashScreen'>;
+
+
+const SplashScreen = ({ navigation }: PropsType) => {
+
+
+    const fadeAnim = new Animated.Value(0); // For fade-in animation
+
+    useEffect(() => {
+        // Fade in animation
+        Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 1500,
+            useNativeDriver: true,
+        }).start();
+
+        // Navigate after 3 seconds
+        const timer = setTimeout(() => {
+            navigation.replace("Home"); // Replace with your screen name
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+     <View style={styles.container}>
+      <Animated.View style={[styles.innerContainer, { opacity: fadeAnim }]}>
+        <Text style={styles.appnametxt}>GO</Text>
+        <Text style={styles.appsecnametxt}>GREEN</Text>
+      </Animated.View>
+    </View>
+    )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: "#447055",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    innerContainer: {
+        flexDirection: "row",
+        gap: 5,
+    },
+    appnametxt: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "#fff",
+        fontFamily: "Poppins-Medium",
+    },
+    appsecnametxt: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "#96d6af",
+        fontFamily: "Poppins-Medium",
+    }
+
+})
