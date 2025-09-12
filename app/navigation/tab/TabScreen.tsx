2ce5f792b363cb0f74ab6ee941b5ba8a96b7cb64
Shaharyar <shaharyar4t4@gmail.com>
2025-09-13 02:34:40 +0500

    TabScreen connect with multiples pages

--- app/navigation/tab/TabScreen.tsx
+++ app/navigation/tab/TabScreen.tsx
@@ -1,8 +1,13 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import About from './About';
 import { Home } from './Home';
 import Profile from './profile';
+
+
 
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
@@ -17,16 +22,34 @@ const TabScreen = () => {
         <Tab.Navigator initialRouteName='Home'>
             <Tab.Screen name="Home" component={Home}
                 options={
-                    { tabBarLabel: 'Home' }
+                    {
+                        tabBarLabel: 'Home',
+                        headerShown: false,
+                        tabBarIcon: () => (
+                            <Feather name="home" size={24} color="black" />
+
+                        ),
+                    }
                 } />
             <Tab.Screen name="About" component={About}
                 options={
-                    { tabBarLabel: 'About' }
+                    {
+                        tabBarLabel: 'About',
+                        headerShown: false,
+                        tabBarIcon: () => (
+
+                            <Entypo name="documents" size={24} color="black" />),
+                    }
                 }
             />
             <Tab.Screen name="Profile" component={Profile}
                 options={
-                    { tabBarLabel: 'Profile' }
+                    { tabBarLabel: 'Profile',
+                        headerShown: false,
+                        tabBarIcon: () => (
+                            <AntDesign name="user" size={24} color="black" />
+                        ),
+                     }
                 }
             />
         </Tab.Navigator>
